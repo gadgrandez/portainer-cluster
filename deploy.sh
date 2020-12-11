d42f4b516e2007b0ed6529ac4ed572ea801c113c
Gad Grandez <gad_grandez@hotmail.com>
2020-12-11 17:00:48 -0500

    Initial

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcloud services enable compute.googleapis.com
+gcloud services enable deploymentmanager.googleapis.com
+gcloud deployment-manager deployments create portainer-cluster --config portainer-cluster.yaml
